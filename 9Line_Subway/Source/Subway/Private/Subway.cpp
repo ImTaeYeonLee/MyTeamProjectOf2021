94664973658f0368657d3488a34bd4a44c59310b
ImTaeYeonLee <90662629+ImTaeYeonLee@users.noreply.github.com>
2021-12-07 22:31:34 +0900

    commit

--- 9Line_Subway/Source/Subway/Private/Subway.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Subway.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Subway, "Subway" );
-
-DEFINE_LOG_CATEGORY(Subway)
